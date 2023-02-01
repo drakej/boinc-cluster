65677d0fdce20c0c59a82b1254497e40f249b6e1
Jonathan Drake <952345+drakej@users.noreply.github.com>
2023-02-01 10:23:07 -0800

    FLASK_ENV was deprecated
    Signed-off-by: Jonathan Drake <952345+drakej@users.noreply.github.com>

--- flask-run.sh
+++ flask-run.sh
@@ -3,6 +3,6 @@
 # You need to setup a virtual environment with Python3 in .env to make this script work correctly
 source .env/bin/activate
 export FLASK_APP=boinccluster.py
-export FLASK_ENV=development
+export FLASK_DEBUG=True
 
 flask run --host=0.0.0.0 --port=8000
